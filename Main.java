bf8e7a337286f2c80fe395504574aca849545be0
jakemckenzie <jake314@uw.edu>
2018-05-09 22:44:26 -0700

    ran tests on each hash to get runtimes. Also fixed get keyData

--- Main.java
+++ Main.java
@@ -18,9 +18,42 @@ public class Main {
      * and the tree codes
      * 
      * @param args The command line arguments
+     * 
+     * Hash function | runtime @ 100      | runtime @ 1000 
+     * joaat_hash    | 780.12 miliseconds | 758.844 miliseconds
+     * bernsteinHash | 752.74 miliseconds | 755.924 miliseconds
+     * djb2          | 669.31 miliseconds | 694.176 miliseconds
+     * FNVHash1      | 673.94 miliseconds | 662.069 miliseconds
      */
     public static void main(String[] args) throws IOException {
-        Long startTime = System.currentTimeMillis();
+        long[] runtime = new long[1000];
+        for (int i = 0; i < 1000; i++) {
+            Long startTime = System.currentTimeMillis();
+            testMain();
+            Long endTime = System.currentTimeMillis();
+            runtime[i] = (endTime - startTime);
+        }
+        double total = 0;
+        for (long d : runtime) total+= (double)d;
+        System.out.println(total / 1000 + " miliseconds");
+        }
+
+        // it = wordSet.iterator();
+        // while (it.hasNext()) System.out.println(temp.get(it.next()));
+        // testMyHashTable();
+    
+
+    public static void testMyHashTable() {
+        MyHashTable<String, String> temp = new MyHashTable<String, String>(1 << 15);
+        temp.put("yes", "please");
+        temp.put("No", "Bugs");
+        System.out.println(temp.get("yes") + temp.get("No"));
+        temp.put("yes", "What do you mean");
+        System.out.println(temp);
+    }
+
+    public static void testMain() throws IOException {
+        
         String s = new String(Files.readAllBytes(Paths.get(WarAndPeace)));
         String[] words = s.split("([^\\w\\Q-\\E\\Q'\\E])+");
         String[] seperators = s.split("([\\w\\Q-\\E\\Q'\\E])+");
@@ -31,28 +64,13 @@ public class Main {
 
         Collections.addAll(wordSet, words);
         Collections.addAll(wordSet, seperators2);
-        //System.out.println(wordSet.size());
+            //System.out.println(wordSet.size());
         MyHashTable<String, String> temp = new MyHashTable<String, String>(1 << 15);
-        // for (int i = 0; i < wordSet.size(); i++) temp.put(wordSet.);
+            // for (int i = 0; i < wordSet.size(); i++) temp.put(wordSet.);
         Iterator<String> it = wordSet.iterator();
         while (it.hasNext()) {
             String bob = it.next();
             temp.put(bob, bob);
         }
-        Long endTime = System.currentTimeMillis();
-        System.out.println((endTime - startTime) + " miliseconds");
-
-        // it = wordSet.iterator();
-        // while (it.hasNext()) System.out.println(temp.get(it.next()));
-        // testMyHashTable();
-    }
-
-    public static void testMyHashTable() {
-        MyHashTable<String, String> temp = new MyHashTable<String, String>(1 << 15);
-        temp.put("yes", "please");
-        temp.put("No", "Bugs");
-        System.out.println(temp.get("yes") + temp.get("No"));
-        temp.put("yes", "What do you mean");
-        System.out.println(temp);
     }
 }
--- MyHashTable.java
+++ MyHashTable.java
@@ -1,7 +1,7 @@
 import java.util.*;
 // cd C:\Users\Epimetheus\Documents\Data Structures\Assignment4
+// javac *.java -Xlint
 public class MyHashTable<K, V> {
-      Random r = new Random();
       int myEntryCount;
       int myBuckets;
       int myCapacity;
@@ -33,7 +33,10 @@ public class MyHashTable<K, V> {
       // }
 
       public void put(K searchKey, V newValue) {
-            int t = bernsteinHash(searchKey);
+            //int t = bernsteinHash(searchKey);
+            //int t = joaat_hash(searchKey);
+            //int t = djb2(searchKey);
+            int t = FNVHash1(searchKey);
             int pos = (t < 0) ? (t % myCapacity) + myCapacity : t % myCapacity;
             int cycle = 0;
                
@@ -53,10 +56,10 @@ public class MyHashTable<K, V> {
          }
 
       public V get(K searchKey) {
-            int t = bernsteinHash(searchKey);
-            int pos = (t < 0) ? (t % myCapacity) + myCapacity : t % myCapacity;
+            // int t = bernsteinHash(searchKey);
+            // int pos = (t < 0) ? (t % myCapacity) + myCapacity : t % myCapacity;
             
-            int cycle = 0;
+            // int cycle = 0;
             keyData myNode = null;
             V temp;
             label:
@@ -85,8 +88,16 @@ public class MyHashTable<K, V> {
       public String toString() {
             StringBuilder temp = new StringBuilder();
             int i = 0;
-            temp.append("{" + myKeys.get(i).myKey.toString() + "=" + myValues.get(myKeys.get(i).myValue));
-            for (i = 1; i < myBuckets; i++) temp.append(", " + myKeys.get(i).myKey.toString() + "=" + myValues.get(myKeys.get(i).myValue));
+            temp.append("{");
+            temp.append(myKeys.get(i).myKey.toString());
+            temp.append("=");
+            temp.append(myValues.get(myKeys.get(i).myValue));
+            for (i = 1; i < myBuckets; i++) {
+                  temp.append(", ");
+                  temp.append(myKeys.get(i).myKey.toString());
+                  temp.append("=");
+                  temp.append(myValues.get(myKeys.get(i).myValue));
+            }
             temp.append("}");
             return temp.toString();
       }
@@ -99,13 +110,47 @@ public class MyHashTable<K, V> {
             return hash;
       }
 
+      public int joaat_hash(K k) {
+            int hash = 0;
+            int i = 0;
+            byte[] key = k.toString().getBytes();
+            while (i != key.length) {
+                hash += (key[i++] & 0xFF);
+                hash += (hash << 0xA);
+                hash ^= (hash >>> 0x6);
+            }
+            hash += (hash << 0x3);
+            hash ^= (hash >>> 0xB);
+            hash += (hash << 0xF);
+            return hash;
+        }
+      public int djb2(K key) {
+            String word = key.toString();
+            int hash = 0;
+            for (int i = 0; i < word.length(); i++) {
+                hash = word.charAt(i) + ((hash << 5) - hash);
+            }
+            return hash;
+      }
+      public int FNVHash1(K key) { 
+            String data = key.toString();
+            final int p = 16777619; 
+            int hash = (int) 2166136261L; 
+            for (int i = 0; i < data.length(); i++) hash = (hash ^ data.charAt(i)) * p; 
+            hash += hash << 13; 
+            hash ^= hash >> 7; 
+            hash += hash << 3; 
+            hash ^= hash >> 17; 
+            hash += hash << 5; 
+            return hash; 
+      }
+
       private keyData getKeyData(K searchKey) {
             keyData temp = null;
-            int i;
+            ArrayList<Integer> random = noRepeatShuffleList(myBuckets);
             label:
-            while (true) {
-                  i = r.nextInt(myKeys.size());
-                  if (myKeys.get(i).myKey.equals(searchKey)) {
+            for (int i = 0; i < myBuckets; i++) {
+                  if (myKeys.get(random.get(i)).myKey.equals(searchKey)) {
                         temp = myKeys.get(i);
                         break label;
                   }
@@ -113,6 +158,15 @@ public class MyHashTable<K, V> {
 
             return temp;
       }
+
+      public ArrayList<Integer> noRepeatShuffleList(int size) {
+            ArrayList<Integer> arr = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                arr.add(i);
+            }
+            Collections.shuffle(arr);
+            return arr;
+        }
       // OOOHHH make a private class for Key values. Each key can have multiple values
       // mapped to it,
       // so each key object needs to keep track of what its pointing to
